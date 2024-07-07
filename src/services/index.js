4fd327262ef1e19b9dbec08f9b23a8308af2462d
Kalpesh Kashyap <kkashyap@ded.abudhabi.ae>
2024-07-07 12:28:21 +0400

    removed api key

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
-const API_KEY = 'LB4CqstSLryzxVakDC80o3LGoqqBZ3jE';
+const API_KEY = 'YOUR API key';
 
 const getAllArticles = async () => {
   const apiRes = await fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`);
