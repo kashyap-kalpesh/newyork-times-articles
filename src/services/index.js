0437cd4970408343acff793f54555af4ec5d4e4b
Kalpesh Kashyap <kkashyap@ded.abudhabi.ae>
2024-07-07 12:30:23 +0400

    read API key from env file

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
-const API_KEY = 'YOUR API key';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const getAllArticles = async () => {
   const apiRes = await fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`);
